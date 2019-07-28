d30ddf7664ff8fb501494827a62a3bd977db618d
Terkion <info@cryptrill.com>
2019-07-28 14:01:28 +0100

    logo and testimonial

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -25,7 +25,7 @@
                   class="testimonial testimonial-style-2 testimonial-with-quotes custom-testimonial-style-1"
                 >
                   <blockquote>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in arcu facilisis quam aliquam ultrices quis in purus. Donec volutpat et justo et viverra. Suspendisse id justo a ipsum viverra ultrices quis et quam.</p>
+                    <p>Using the ABJ form was a high help to me, as i lost the drive to read, but after been accessed a couple of times, i pickd up and now i'm sure of my result.</p>
                   </blockquote>
                   <div class="testimonial-author">
                     <img
@@ -53,7 +53,63 @@
                   class="testimonial testimonial-style-2 testimonial-with-quotes custom-testimonial-style-1"
                 >
                   <blockquote>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in arcu facilisis quam aliquam ultrices quis in purus. Donec volutpat et justo et viverra. Suspendisse id justo a ipsum viverra ultrices quis et quam.</p>
+                    <p>Coming into the University, i was scared until i heard abt the freshers orentation programme. It helped me to understand how the school system runs. Thumbs up.</p>
+                  </blockquote>
+                  <div class="testimonial-author">
+                    <img
+                      src="img/demos/education/authors/author-1.jpg"
+                      class="img-fluid rounded-circle"
+                      alt
+                    />
+                    <p>
+                      <strong class="text-uppercase text-color-dark">ALICE DOE</strong>
+                    </p>
+                    <div class="rate">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row justify-content-center">
+              <div class="col-lg-10">
+                <div
+                  class="testimonial testimonial-style-2 testimonial-with-quotes custom-testimonial-style-1"
+                >
+                  <blockquote>
+                    <p>Depressed afer my 2nd year in school, i ran into a friend in this club and i was assigned a man, to cut long story short, now i have hope for the fututre and its all thanks to yougenius club.</p>
+                  </blockquote>
+                  <div class="testimonial-author">
+                    <img
+                      src="img/demos/education/authors/author-1.jpg"
+                      class="img-fluid rounded-circle"
+                      alt
+                    />
+                    <p>
+                      <strong class="text-uppercase text-color-dark">ALICE DOE</strong>
+                    </p>
+                    <div class="rate">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row justify-content-center">
+              <div class="col-lg-10">
+                <div
+                  class="testimonial testimonial-style-2 testimonial-with-quotes custom-testimonial-style-1"
+                >
+                  <blockquote>
+                    <p>The timely provision of the several tutorial classes helped me to excel and exceed my prees. thank you so much.</p>
                   </blockquote>
                   <div class="testimonial-author">
                     <img
