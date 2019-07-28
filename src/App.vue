6c1d51bc27c82546c6056801a442b10751bb6a1f
Augment Chinedu <augmentchinedu@gmail.com>
2019-07-28 11:55:36 +0100

    Inner Sections Added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
+    <Header />
+    <div role="main" class="main">
+      <Slider />
+      <Intro />
+    </div>
     <Footer />
   </div>
 </template>
 
 <script>
 import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import Slider from './components/Slider.vue'
+import Intro from './components/Intro.vue'
+import GetStarted from './components/GetStarted.vue'
 
 export default {
   name: 'app',
@@ -14,16 +23,36 @@ export default {
     script:[
       {src: 'master/style-switcher/style.switcher.localstorage.js'},
       {src: 'vendor/modernizr/modernizr.min.js'},
-      {src: 'vendor/jquery/jquery.min.js'},
+      {src: 'vendor/jquery/jquery.min.js', async: true},
       {src: 'vendor/jquery.appear/jquery.appear.min.js', defer: true, body: true},
       {src: 'vendor/jquery.easing/jquery.easing.min.js', defer: true, body: true},
       {src: 'vendor/jquery.cookie/jquery.cookie.min.js', defer: true, body: true},
       {src: 'master/style-switcher/style.switcher.js', id: 'styleSwitcherScript', defer: true, body: true},
-      {src: 'vendor/popper/umd/popper.min.js', defer: true, body: true}
+      {src: 'vendor/popper/umd/popper.min.js', defer: true, body: true},
+      {src: 'vendor/bootstrap/js/bootstrap.min.js', defer: true, body: true},
+      {src: 'vendor/common/common.min.js', defer: true, body: true},
+      {src: 'vendor/jquery.validation/jquery.validate.min.js', defer: true, body: true},
+      {src: 'vendor/jquery.gmap/jquery.gmap.min.js', defer: true, body: true},
+      {src: 'vendor/jquery.lazyload/jquery.lazyload.min.js', defer: true, body: true},
+      {src: 'vendor/isotope/jquery.isotope.min.js', defer: true, body: true},
+      {src: 'vendor/magnific-popup/jquery.magnific-popup.min.js', defer: true, body: true},
+      {src: 'vendor/vide/jquery.vide.min.js', defer: true, body: true},
+      {src: 'vendor/vivus/vivus.min.js', defer: true, body: true},
+      {src: 'vendor/jquery.countdown/jquery.countdown.min.js', defer: true, body: true},
+      {src: 'js/theme.js', defer: true, body: true},
+      {src: 'vendor/rs-plugin/js/jquery.themepunch.tools.min.js', defer: true, body: true},
+      {src: 'vendor/rs-plugin/js/jquery.themepunch.revolution.min.js', defer: true, body: true},
+      {src: 'js/views/view.contact.js', defer: true, body: true},
+      {src: 'js/demos/demo-education.js', defer: true, body: true},
+      {src: 'js/theme.init.js', defer: true, body: true}
     ]
-  },
+ },
   components: {
-    Footer
+    Footer,
+    Header,
+    Slider,
+    Intro,
+    GetStarted
   }
 }
 </script>
---
+++ src/components/Intro.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="container" id="main">
+    <div class="row text-center pb-4 pt-5 my-5">
+      <div class="col-md-10 mx-md-auto">
+        <h1
+          class="word-rotator slide font-weight-bold text-8 mb-3 appear-animation"
+          data-appear-animation="fadeInUpShorter"
+        >
+          <span>Porto is</span>
+          <span class="word-rotator-words bg-dark">
+            <b class="is-visible">incredibly</b>
+            <b>especially</b>
+            <b>extremely</b>
+          </span>
+          <span>beautiful and fully responsive.</span>
+        </h1>
+        <p
+          class="lead appear-animation mb-0"
+          data-appear-animation="fadeInUpShorter"
+          data-appear-animation-delay="300"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum, nulla vel pellentesque consequat, ante nulla hendrerit arcu, ac tincidunt mauris lacus sed leo.</p>
+      </div>
+    </div>
+    <div
+      class="row pb-5 mb-5 appear-animation"
+      data-appear-animation="fadeInUpShorter"
+      data-appear-animation-delay="300"
+    >
+      <div class="col-lg-8">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="feature-box feature-box-style-2 mb-3">
+              <div class="feature-box-icon">
+                <i class="icons icon-support text-color-primary"></i>
+              </div>
+              <div class="feature-box-info">
+                <h4 class="font-weight-bold text-4 mb-0">Customer Support</h4>
+                <p
+                  class="mb-4"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+              </div>
+            </div>
+            <div class="feature-box feature-box-style-2 mb-3">
+              <div class="feature-box-icon">
+                <i class="icons icon-doc text-color-primary"></i>
+              </div>
+              <div class="feature-box-info">
+                <h4 class="font-weight-bold text-4 mb-0">HTML5 / CSS3 / JS</h4>
+                <p
+                  class="mb-4"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+              </div>
+            </div>
+            <div class="feature-box feature-box-style-2">
+              <div class="feature-box-icon">
+                <i class="icons icon-social-google text-color-primary"></i>
+              </div>
+              <div class="feature-box-info">
+                <h4 class="font-weight-bold text-4 mb-0">500+ Google Fonts</h4>
+                <p
+                  class="mb-4"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="feature-box feature-box-style-2 mb-3">
+              <div class="feature-box-icon">
+                <i class="icons icon-layers text-color-primary"></i>
+              </div>
+              <div class="feature-box-info">
+                <h4 class="font-weight-bold text-4 mb-0">Sliders</h4>
+                <p
+                  class="mb-4"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+              </div>
+            </div>
+            <div class="feature-box feature-box-style-2 mb-3">
+              <div class="feature-box-icon">
+                <i class="icons icon-user text-color-primary"></i>
+              </div>
+              <div class="feature-box-info">
+                <h4 class="font-weight-bold text-4 mb-0">Icons</h4>
+                <p
+                  class="mb-4"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+              </div>
+            </div>
+            <div class="feature-box feature-box-style-2">
+              <div class="feature-box-icon">
+                <i class="icons icon-menu text-color-primary"></i>
+              </div>
+              <div class="feature-box-info">
+                <h4 class="font-weight-bold text-4 mb-0">Buttons</h4>
+                <p
+                  class="mb-4"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="accordion accordion-modern accordion-modern-grey-scale-1" id="accordion">
+          <div class="card card-default">
+            <div class="card-header">
+              <h4 class="card-title m-0">
+                <a
+                  class="accordion-toggle text-color-dark font-weight-bold"
+                  data-toggle="collapse"
+                  data-parent="#accordion"
+                  href="#collapseOne"
+                >
+                  <i class="icons icon-diamond text-color-primary"></i>
+                  Creative Websites
+                </a>
+              </h4>
+            </div>
+            <div id="collapseOne" class="collapse show">
+              <div class="card-body text-2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blanorem ipsum dolor sit amet, consecte.</p>
+                <p class="mb-0">
+                  Adipiscing elit phasellus blanit ma...
+                  <a href="#" class="text-color-dark font-weight-semibold ml-2">
+                    read more
+                    <i class="fas fa-angle-right position-relative top-1 ml-1"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="card card-default">
+            <div class="card-header">
+              <h4 class="card-title m-0">
+                <a
+                  class="accordion-toggle text-color-dark font-weight-bold"
+                  data-toggle="collapse"
+                  data-parent="#accordion"
+                  href="#collapseTwo"
+                >
+                  <i class="icons icon-bubble text-color-primary"></i>
+                  Contact Forms
+                </a>
+              </h4>
+            </div>
+            <div id="collapseTwo" class="collapse">
+              <div class="card-body text-2">
+                <p
+                  class="mb-0"
+                >Donec tellus massa, tristique sit amet condimentum vel, facilisis quis sapien.</p>
+              </div>
+            </div>
+          </div>
+          <div class="card card-default">
+            <div class="card-header">
+              <h4 class="card-title m-0">
+                <a
+                  class="accordion-toggle text-color-dark font-weight-bold"
+                  data-toggle="collapse"
+                  data-parent="#accordion"
+                  href="#collapseThree"
+                >
+                  <i class="icons icon-grid text-color-primary"></i>
+                  Portfolio Pages
+                </a>
+              </h4>
+            </div>
+            <div id="collapseThree" class="collapse">
+              <div class="card-body text-2">
+                <p
+                  class="mb-0"
+                >Donec tellus massa, tristique sit amet condimentum vel, facilisis quis sapien.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+opacity: 1;
+</style>
